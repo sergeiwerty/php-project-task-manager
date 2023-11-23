ac90805a9808813e5929373ed778e53d20260c53
sergeiwerty <dshl.srp@gmail.com>
2023-11-23 15:17:17 +0200

    added general validation rules & corrected create.blade.php & added edit.blade.php

--- resources/views/taskStatus/create.blade.php
+++ resources/views/taskStatus/create.blade.php
@@ -2,22 +2,13 @@
 
 @section('content')
     <div class="grid col-span-full">
-        <h1 class="mb-5">Создать статус</h1>
+        <h1 class="mb-5">{{__('taskStatus.Create status')}}</h1>
         {{ Form::model($taskStatus, ['url' => route('task_statuses.store'), 'class' => 'w-50']) }}
         @csrf
         <div class="flex flex-col">
             @include('taskStatus.form')
-            @if($errors->any())
-                <div class="text-rose-600">
-                    @foreach($errors->all() as $error)
-                        {{ $error }}
-                    @endforeach
-                </div>
-            @endif
-            @include('flash::message')
-
             <div class="mt-5">
-                {{ Form::submit('Создать', ['class' => "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"])}}
+                {{ Form::submit(__('taskStatus.Create'), ['class' => "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"])}}
             </div>
         </div>
         {{ Form::close() }}
---
+++ resources/views/taskStatus/edit.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="grid col-span-full">
+        <h1 class="mb-5">{{ __('taskStatus.Status edit') }}</h1>
+        {{ Form::model($taskStatus, ['url' => route('task_statuses.update', $taskStatus), 'method' => 'PATCH', 'class' => 'w-50']) }}
+        @csrf
+        <div class="flex flex-col">
+            @include('taskStatus.form')
+            <div class="mt-5">
+                {{ Form::submit(__('taskStatus.Update'), ['class' => "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"])}}
+            </div>
+        </div>
+        {{ Form::close() }}
+    </div>
+@endsection('content')
