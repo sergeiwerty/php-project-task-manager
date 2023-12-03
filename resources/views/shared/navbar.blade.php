998b69ec2e699de4683c18ae4864a8042c46153f
sergeiwerty <dshl.srp@gmail.com>
2023-12-03 23:52:21 +0200

    added link to tasks page on the navbar in navbar.blade.php

--- resources/views/shared/navbar.blade.php
+++ resources/views/shared/navbar.blade.php
@@ -25,8 +25,7 @@
             <div class="items-center justify-between hidden w-full lg:flex lg:w-auto lg:order-1">
                 <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                     <li>
-{{--                        <a class="block py-2 pl-3 pr-4 text-gray-700 hover:text-blue-700 lg:p-0" href="{{ route('tasks.index') }}">Задачи</a>--}}
-                        <a class="block py-2 pl-3 pr-4 text-gray-700 hover:text-blue-700 lg:p-0" href="">Задачи</a>
+                        <a class="block py-2 pl-3 pr-4 text-gray-700 hover:text-blue-700 lg:p-0" href="{{ route('tasks.index') }}">Задачи</a>
                     </li>
                     <li>
                         <a class="block py-2 pl-3 pr-4 text-gray-700 hover:text-blue-700 lg:p-0" href="{{ route('task_statuses.index') }}">{{__('taskStatus.Statuses')}}</a>
